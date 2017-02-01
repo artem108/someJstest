8020bd54cfc6a6d497d91d030460b96e763f2b3b
Artem Arshaniy <artem.arshaniy108@gmail.com>
2017-02-01 23:09:32 +0200

    add button edit save

--- js/script.js
+++ js/script.js
@@ -1,20 +1,54 @@
 document.body.onload = function() {
+// take value input and create item
+
+var newLi, list;
+
+list = document.getElementById('list');
+newLi = document.createElement('li');
 
 document.getElementById('button').onclick = function addText() {
-
-var newText, newLi, list;
-newText = document.getElementById('input').value;
-newLi = document.createElement('li');
-list = document.getElementById('list');
+var newText = document.getElementById('input').value;
 
 list.appendChild(newLi);
-
 newLi.innerHTML = newText;
 
+}
 
+// create button for li
 
+newLi.onclick = function createButton() {
+  // delete
+  var removeLi = document.createElement('input');
 
-// document.getElementById('areaForText').innerHTML = newText;
- // return addText;
-}}
-// пригодиться document.createElement(tag)
+  removeLi.id = 'removeItem'
+  removeLi.type = 'button'
+  removeLi.value = 'delete'
+
+  newLi.appendChild(removeLi);
+  // fuction for delete item
+  removeLi.onclick = function remove() {
+
+    list.removeChild(newLi);
+
+  }
+  // edit button
+  var edit = document.createElement('input');
+
+  edit.id = 'edit'
+  edit.type = 'button'
+  edit.value = 'edit'
+
+  newLi.appendChild(edit);
+
+  edit.onclick = function edit() {
+    // save change button
+    var save = document.createElement('input');
+    save.id = 'save'
+    save.type = 'button'
+    save.value = 'save'
+    newLi.appendChild(save);
+
+  }
+
+}
+}
